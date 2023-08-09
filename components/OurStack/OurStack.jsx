271efb7e7aada5d18da5476c661f130e4d598d7f
Artur <yeggart1988@gmail.com>
2023-08-09 10:50:32 +0400

    fix

--- components/OurStack/OurStack.jsx
+++ components/OurStack/OurStack.jsx
@@ -1,120 +1,120 @@
 export default function OurStack() {
   return (
     <section className="services-section section-padding">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="section-title">
-                <h6>What We Provide</h6>
-                <h2>IT Solutions</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="section-title">
+              <h6>What We Provide</h6>
+              <h2>IT Solutions</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-mobile-app"></i>
+              </div>
+              <h3>Mobile App</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-mobile-app"></i>
-                </div>
-                <h3>Mobile App</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-programming"></i>
+              </div>
+              <h3>Web Development</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-programming"></i>
-                </div>
-                <h3>Web Development</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-innovation"></i>
+              </div>
+              <h3>AI Models</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-cpu-1"></i>
-                </div>
-                <h3>AI Models</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-coding"></i>
+              </div>
+              <h3>DevOps</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-coding"></i>
-                </div>
-                <h3>DevOps</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-project-management"></i>
+              </div>
+              <h3>Consulting</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-project-management"></i>
-                </div>
-                <h3>Consulting</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-services-item">
+              <div className="services-icon">
+                <i className="flaticon-puzzle"></i>
               </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="single-services-item">
-                <div className="services-icon">
-                  <i className="flaticon-cloud"></i>
-                </div>
-                <h3>UI/UX</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt dolore magna aliqua
-                </p>
-                <div className="services-btn-link">
-                  <a href="#" className="services-link">
-                    Read More
-                  </a>
-                </div>
+              <h3>UI/UX</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt dolore magna aliqua
+              </p>
+              <div className="services-btn-link">
+                <a href="#" className="services-link">
+                  Read More
+                </a>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   )
 }

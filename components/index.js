6237c9107ad009efa7c5a5dfa7cef1d63035ce02
NarekAvagyan777 <narek.avagyan.web@gmail.com>
2023-08-17 10:33:48 +0400

    deleted files

--- components/index.js
+++ components/index.js
@@ -2,17 +2,12 @@ import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
 import Layout from "./Layout/Layout"
 import AboutCompany from "./AboutCompany/AboutCompany"
-import Achievements from "./Achievements/Achievements"
 import Blog from "./Blog/Blog"
-import ChooseUs from "./ChooseUs/ChooseUs"
-import ClientsSay from "./ClientsSay/ClientsSay"
-import CooperationBlock from "./CooperationBlock/CooperationBlock"
 import OurCustomers from "./OurCustomers/OurCustomers"
 import OurStack from "./OurStack/OurStack"
 import PageTitle from "./PageTitle/PageTitle"
 import SliderHeader from "./SliderHeader/SliderHeader"
 import School from "./School/School"
-import TrustUs from "./TrustUs/TrustUs"
 import ContactUs from "./ContactUs/ContactUs"
 import StartUp from "./StartUp/StartUp"
 
@@ -21,17 +16,12 @@ export {
     Footer,
     Layout,
     AboutCompany,
-    Achievements,
     Blog,
-    ChooseUs,
-    ClientsSay,
-    CooperationBlock,
     OurCustomers,
     OurStack,
     PageTitle,
     SliderHeader,
     School,
-    TrustUs,
     ContactUs,
     StartUp
 }

1cc9155f723ccabab93d502307e3addbbf09667a
NarekAvagyan777 <narek.avagyan.web@gmail.com>
2023-08-18 16:09:28 +0400

    added our startups

--- pages/company.js
+++ pages/company.js
@@ -31,70 +31,44 @@ const teamMembers = [
   },
 ];
 
-// function TeamMember({ member }) {
-//   const { name, role, image, socialLinks } = member;
-
-//   return (
-//     <div className="single-team-box">
-//       <div className="team-image">
-//         <img src={image} alt="team" />
-//         <div className="team-social-icon">
-//           <a href={socialLinks.facebook} className="social-color-1">
-//             <i className="fab fa-facebook-f"></i>
-//           </a>
-//           <a href={socialLinks.twitter} className="social-color-2">
-//             <i className="fab fa-twitter"></i>
-//           </a>
-//           <a href={socialLinks.linkedin} className="social-color-3">
-//             <i className="fab fa-linkedin"></i>
-//           </a>
-//         </div>
-//       </div>
-//       <div className="team-info">
-//         <h3>{name}</h3>
-//         <span>{role}</span>
-//       </div>
-//     </div>
-//   );
-// }
 
 function Company() {
   let router = useRouter();
 
   const asPath = router.asPath;
 
-  // useEffect(() => {
-  //   (() => {
-  //     if (asPath.includes("#")) {
-  //       const last = asPath.split("#")[1];
-  //       const navbar = document.getElementsByClassName("navbar-area")[0];
+  useEffect(() => {
+    (() => {
+      if (asPath.includes("#")) {
+        const last = asPath.split("#")[1];
+        const navbar = document.getElementsByClassName("navbar-area")[0];
 
-  //       if (last === "aboutus") {
-  //         const element = document.getElementsByClassName('about-area')[0];
-  //         const rect =
-  //           element.getBoundingClientRect().top +
-  //           window.pageYOffset -
-  //           navbar.clientHeight;
-  //         window.scrollTo({ top: rect, behavior: "smooth" });
-  //       } else if (last === "team") {
-  //         const element = document.getElementsByClassName("team-area")[0];
-  //         const rect =
-  //           element.getBoundingClientRect().top +
-  //           window.pageYOffset -
-  //           navbar.clientHeight;
-  //         window.scrollTo({ top: rect, behavior: "smooth" });
-  //       } else if (last === "careers") {
-  //         const element = document.getElementById("careersss");
-  //         const rect =
-  //           element.getBoundingClientRect().top +
-  //           window.pageYOffset -
-  //           navbar.clientHeight;
-  //         window.scrollTo({ top: rect, behavior: "smooth" });
-  //       }
-  //     }
-  //   })();
+        if (last === "aboutus") {
+          const element = document.getElementsByClassName('about-area')[0];
+          const rect =
+            element.getBoundingClientRect().top +
+            window.pageYOffset -
+            navbar.clientHeight;
+          window.scrollTo({ top: rect, behavior: "smooth" });
+        } else if (last === "team") {
+          const element = document.getElementsByClassName("team-area")[0];
+          const rect =
+            element.getBoundingClientRect().top +
+            window.pageYOffset -
+            navbar.clientHeight;
+          window.scrollTo({ top: rect, behavior: "smooth" });
+        } else if (last === "careers") {
+          const element = document.getElementById("careersss");
+          const rect =
+            element.getBoundingClientRect().top +
+            window.pageYOffset -
+            navbar.clientHeight;
+          window.scrollTo({ top: rect, behavior: "smooth" });
+        }
+      }
+    })();
 
-  // }, [asPath]);
+  });
 
   return (
     <>
